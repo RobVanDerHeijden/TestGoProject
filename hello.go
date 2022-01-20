1121b44b8659aeeb2522add3c67783c4c5abfb2e
Heijden,Rob R.M.H.W. van der <rmhw.vanderheijden@student.fontys.nl>
2022-01-20 15:01:37 +0100

    Basic variables and if statement
    Trying out shorttag syntax and string concatenation

--- hello.go
+++ hello.go
@@ -1,14 +1,34 @@
 package main
 
+// Importing Packaging (fmt is a basic package for I/O text)
 import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World2!")
-	var x int = 5
-	var y int = 8
-	var sum int = x + y
+	// Basic Print function
+	fmt.Println("Hello, World3!")
 
-	fmt.Println(sum)
+	// Variables with short tag syntax   :=
+	x := 6
+	y := 1
+	sum := x + y
+	sumEvaluationText := ""
+	sumTextOutput := ""
+
+	// If statement + if else + else
+	if sum > 8 {
+		sumEvaluationText = "is higher then 8"
+	} else if sum < 8 {
+		sumEvaluationText = "is lower then 8"
+	} else {
+		sumEvaluationText = "is equal to 8"
+	}
+
+	// Sprintf for string/int concatenation (%d = int, %s = string)
+	sumTextOutput = fmt.Sprintf("%d: %s", sum, sumEvaluationText)
+	fmt.Println(sumTextOutput)
+
+	//fmt.Println(sum)
+
 }
