60be09672110c4e0e7caa81d37eb0fa016926ab4
Heijden,Rob R.M.H.W. van der <rmhw.vanderheijden@student.fontys.nl>
2022-01-20 16:12:58 +0100

    Array Types, Loops, Functions
    Different array like types, different loop methods, functions calling with errors (since exceptions aren't in Go

--- hello.go
+++ hello.go
@@ -2,12 +2,14 @@ package main
 
 // Importing Packaging (fmt is a basic package for I/O text)
 import (
+	"errors"
 	"fmt"
+	"math"
 )
 
 func main() {
 	// Basic Print function
-	fmt.Println("Hello, World3!")
+	//fmt.Println("Hello, World3!")
 
 	// Variables with short tag syntax   :=
 	x := 6
@@ -29,6 +31,70 @@ func main() {
 	sumTextOutput = fmt.Sprintf("%d: %s", sum, sumEvaluationText)
 	fmt.Println(sumTextOutput)
 
-	//fmt.Println(sum)
+	// Array (fixed lenght)
+	arrayExample := [5]int{3, 9, 46, 842, 31}
+	fmt.Println(arrayExample)
 
+	// Slice (no fixxed lenght)
+	sliceExample := []int{3, 9, 46, 842, 31}
+	sliceExample = append(sliceExample, 357)
+	fmt.Println(sliceExample)
+
+	// Map -> map[keys]values
+	mapExample := make(map[string]int)
+	mapExample["apples"] = 13
+	mapExample["bananas"] = 5
+	mapExample["pineapples"] = 2
+	fmt.Println(mapExample)
+	fmt.Println(mapExample["apples"])
+	delete(mapExample, "bananas")
+	fmt.Println(mapExample)
+
+	// Loop (for loop)
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	// Loop (while loop)
+	i := 0
+	for i < 3 {
+		fmt.Println(i)
+		i++
+	}
+	// Loop (Array: iterate over each element)
+	arrayIterateExample := [5]string{"yes", "no", "maybe", "perhps", "of course"}
+	for index, value := range arrayIterateExample {
+		fmt.Println("index", index, "value", value)
+	}
+	// Loop (Map: iterate over each element)
+	mapIterateExample := make(map[string]string)
+	mapIterateExample["chocolate"] = "Delicious"
+	mapIterateExample["strawbarries"] = "also good"
+	for key, value := range mapIterateExample {
+		fmt.Println("key", key, "value", value)
+	}
+
+	// Call outside basic additionfunction
+	resultSumCalc := sumCalc(314, 159)
+	fmt.Println(resultSumCalc)
+
+	// Call outside square root function
+	resultsqrtCalc, err := sqrtCalc(25)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resultsqrtCalc)
+	}
+
+}
+
+// Seperate function basic addition
+func sumCalc(x int, y int) int {
+	return x + y
+}
+
+func sqrtCalc(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Undifined for negative numbers")
+	}
+	return math.Sqrt(x), nil
 }
